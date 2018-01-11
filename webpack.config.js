983223c5c34eb03eb6843cac70b74000ff257014
Anthony-Wilson <liuyutian_web@163.com>
2018-01-11 16:12:49 +0800

    配置开发环境

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    entry:[
+        "babel-polyfill",
+        "index.js"
+    ],
+    output: {
+        path: __dirname + "./dist/",
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /.css$/,
+                exclude: /(node_modules|bower_components)/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+}
