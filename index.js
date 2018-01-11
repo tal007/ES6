1296ff03973c0b1e73e097e99d1db923cc68fde0
Anthony-Wilson <liuyutian_web@163.com>
2018-01-11 19:19:50 +0800

    使用let与const

---
+++ index.js
@@ -0,0 +1,49 @@
+/**
+ * 作业：刘玉田
+ * 邮箱：liuyutian_web@163.com
+ * 博客：http://www.lytblog.com
+ */
+
+//  let and const
+let oWrap = document.body;
+{
+    let a = "1";
+    // 无法多次使用 let 定义一个变量 http://localhost:8080/
+    // let a = "2";
+    oWrap.innerHTML += a;
+}
+// 无法获取定义在一个 {}里面的变量
+// Uncaught ReferenceError: a is not defined
+// console.log(a)
+{
+    // 这里使用 let定义变量 a不会报错
+    // 使用 let 会有自己的作用域 {}，{} 与 {}之间的变量互不影响
+    let a = "2";
+    oWrap.innerHTML += a;
+}
+{
+    let a = [];
+    // for 循环中使用 let 实现每一次都是这个循环变量
+    for (let i = 0; i < 10; i++) {
+        a[i] = function () { oWrap.innerHTML += `<br>使用let定义变量的for循环----${i}`}
+    }
+    a.forEach(function(item){
+        item();
+    })
+}
+{
+    // for循环还有一个特别之处，就是设置循环变量的那部分是一个父作用域，而循环体内部是一个单独的子作用域。
+    /* 
+        这个代码在未编译的时候可以循环 3次
+        但是在使用webpack编译后只有一次 因为 webpack编辑就是把 let 换为 var 了
+    */
+    for (let i = 0; i < 3; i++) {
+        let i = 'abc';
+        console.log(i);
+    }
+}
+{
+    // 使用 let 不存在变量提升 依然编译为 es5的 var
+    console.log(a)
+    let a = "1";
+}
